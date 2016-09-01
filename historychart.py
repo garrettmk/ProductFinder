7cd74e9b9be62e40883cf9ee6baced15ac72eb8e
Garrett Myrick <garrettmyrick@gmail.com>
2016-09-01 14:44:46 -0700

    Set HistoryChart to only load the last week's worth of data automatically. Previous data is loaded on demand, as the user scrolls the timeline back. Reworked the resetAxis() function. Each category in the chart is represented by a separate series for points and lines.

--- historychart.py
+++ historychart.py
@@ -1,5 +1,3 @@
-import math
-
 from PyQt5.QtCore import *
 from PyQt5.QtChart import *
 from PyQt5.QtGui import *
@@ -41,68 +39,92 @@ class ProductHistoryChart(QChart):
 
         self.model = None
 
+        rcolor = QColor(Qt.darkCyan)
+        pcolor = QColor(Qt.darkGreen)
+        ocolor = QColor(Qt.darkMagenta)
+
         # Create the axes and add them to the chart
         self.timeAxis = QDateTimeAxis()
         self.timeAxis.setFormat('M/dd hh:mm')
         self.timeAxis.setTitleText('Date/Time')
         self.addAxis(self.timeAxis, Qt.AlignBottom)
+        self.timeAxis.minChanged.connect(self.loadHistoryUntil)
 
         self.rankAxis = QValueAxis()
         self.rankAxis.setLabelFormat('%\'i')
         self.rankAxis.setTitleText('Sales Rank')
+        self.rankAxis.setLinePenColor(rcolor)
+        self.rankAxis.setLabelsColor(rcolor)
         self.addAxis(self.rankAxis, Qt.AlignLeft)
 
         self.priceAxis = QValueAxis()
         self.priceAxis.setLabelFormat('$%.2f')
         self.priceAxis.setTitleText('Price')
+        self.priceAxis.setLinePenColor(pcolor)
+        self.priceAxis.setLabelsColor(pcolor)
         self.addAxis(self.priceAxis, Qt.AlignRight)
 
         self.offersAxis = QValueAxis()
         self.offersAxis.setLabelFormat('%i')
         self.offersAxis.setTitleText('Offers')
+        self.offersAxis.setLinePenColor(ocolor)
+        self.offersAxis.setLabelsColor(ocolor)
         self.addAxis(self.offersAxis, Qt.AlignRight)
 
         # Create the series, add them to the chart, and attach the axes.
-        self.rankSeries = QLineSeries()
-        self.addSeries(self.rankSeries)
-        self.rankSeries.attachAxis(self.timeAxis)
-        self.rankSeries.attachAxis(self.rankAxis)
-        #self.rankSeries.setPointsVisible(True)
-        self.rankAxis.setLinePenColor(self.rankSeries.color())
-        self.rankAxis.setLabelsColor(self.rankSeries.color())
+        self.rankLine = QLineSeries()
+        self.addSeries(self.rankLine)
+        self.rankLine.attachAxis(self.timeAxis)
+        self.rankLine.attachAxis(self.rankAxis)
+        self.rankLine.setColor(rcolor)
 
-        self.priceSeries = QLineSeries()
-        self.addSeries(self.priceSeries)
-        self.priceSeries.attachAxis(self.priceAxis)
-        self.priceSeries.attachAxis(self.timeAxis)
-        #self.priceSeries.setPointsVisible(True)
-        self.priceAxis.setLinePenColor(self.priceSeries.color())
-        self.priceAxis.setLabelsColor(self.priceSeries.color())
+        self.rankPoints = QScatterSeries()
+        self.addSeries(self.rankPoints)
+        self.rankPoints.attachAxis(self.timeAxis)
+        self.rankPoints.attachAxis(self.rankAxis)
+        self.rankPoints.setPen(QPen(Qt.NoPen))
+        self.rankPoints.setColor(rcolor)
+        self.rankPoints.setMarkerSize(6)
+        self.rankPoints.setPointsVisible(True)
 
-        self.offerLineSeries = QLineSeries()
-        self.addSeries(self.offerLineSeries)
-        self.offerLineSeries.attachAxis(self.offersAxis)
-        self.offerLineSeries.attachAxis(self.timeAxis)
-        self.offersAxis.setLinePenColor(self.offerLineSeries.color())
-        self.offersAxis.setLabelsColor(self.offerLineSeries.color())
+        self.priceLine = QLineSeries()
+        self.addSeries(self.priceLine)
+        self.priceLine.attachAxis(self.priceAxis)
+        self.priceLine.attachAxis(self.timeAxis)
+        self.priceLine.setColor(pcolor)
 
-        # self.offerPointSeries = QScatterSeries()
-        # self.addSeries(self.offerPointSeries)
-        # self.offerPointSeries.setPen(QPen(Qt.NoPen))
-        # self.offerPointSeries.setColor(self.offerLineSeries.color())
-        # self.offerPointSeries.setMarkerSize(6)
-        # self.offerPointSeries.attachAxis(self.offersAxis)
-        # self.offerPointSeries.attachAxis(self.timeAxis)
-        # self.offerPointSeries.setPointsVisible(True)
+        self.pricePoints = QScatterSeries()
+        self.addSeries(self.pricePoints)
+        self.pricePoints.attachAxis(self.priceAxis)
+        self.pricePoints.attachAxis(self.timeAxis)
+        self.pricePoints.setPen(QPen(Qt.NoPen))
+        self.pricePoints.setColor(pcolor)
+        self.pricePoints.setMarkerSize(6)
+        self.pricePoints.setPointsVisible(True)
+
+        self.offerLine = QLineSeries()
+        self.addSeries(self.offerLine)
+        self.offerLine.attachAxis(self.offersAxis)
+        self.offerLine.attachAxis(self.timeAxis)
+        self.offerLine.setColor(ocolor)
+
+        self.offerPoints = QScatterSeries()
+        self.addSeries(self.offerPoints)
+        self.offerPoints.setPen(QPen(Qt.NoPen))
+        self.offerPoints.setColor(ocolor)
+        self.offerPoints.setMarkerSize(6)
+        self.offerPoints.attachAxis(self.offersAxis)
+        self.offerPoints.attachAxis(self.timeAxis)
+        self.offerPoints.setPointsVisible(True)
 
         # Chart set-up
         self.legend().hide()
         self.setFlags(QGraphicsItem.ItemIsFocusable | QGraphicsItem.ItemIsSelectable)
         self.acceptHoverEvents()
 
-        self.rankSeries.hovered.connect(self.seriesHovered)
-        self.priceSeries.hovered.connect(self.seriesHovered)
-        self.offerLineSeries.hovered.connect(self.seriesHovered)
+        self.rankPoints.hovered.connect(self.seriesHovered)
+        self.pricePoints.hovered.connect(self.seriesHovered)
+        self.offerPoints.hovered.connect(self.seriesHovered)
         self.callout = Callout(self)
 
     def seriesHovered(self, point, show):
@@ -110,11 +132,11 @@ class ProductHistoryChart(QChart):
             timestamp = QDateTime.fromMSecsSinceEpoch(point.x()).toString('M/d H:mm')
 
             series = self.sender()
-            if series is self.rankSeries:
+            if series is self.rankPoints:
                 value = 'Rank: {:,}'.format(int(point.y()))
-            elif series is self.priceSeries:
+            elif series is self.pricePoints:
                 value = 'Price: ${:,.2f}'.format(point.y())
-            elif series is self.offerLineSeries:
+            elif series is self.offerPoints :
                 value = 'Offers: {:,}'.format(int(point.y()))
             else:
                 value = ':-('
@@ -133,45 +155,112 @@ class ProductHistoryChart(QChart):
         self.model.modelReset.connect(self.modelUpdated)
 
     def modelUpdated(self):
-        self.rankSeries.clear()
-        self.priceSeries.clear()
-        self.offerLineSeries.clear()
-        #self.offerPointSeries.clear()
+        self.rankLine.clear()
+        self.rankPoints.clear()
+        self.priceLine.clear()
+        self.pricePoints.clear()
+        self.offerLine.clear()
+        self.offerPoints.clear()
 
-        for row in range(self.model.rowCount()):
-            record = self.model.record(row)
+        last = self.model.record(0).value('Timestamp')
+        if last:
+            time = QDateTime.fromTime_t(last, Qt.LocalTime).addDays(-7)
+        else:
+            time = QDateTime.currentDateTimeUtc().addDays(-7)
 
-            time = QDateTime.fromTime_t(record.value('Timestamp'), Qt.UTC)
-            time = time.toLocalTime().toMSecsSinceEpoch()
-
-            self.rankSeries.append(time, record.value('SalesRank'))
-            self.priceSeries.append(time, record.value('Price'))
-            #self.offerPointSeries.append(time, record.value('Offers'))
-
-            prev = self.offerLineSeries.at(self.offerLineSeries.count() - 1)
-            prev.setX(time)
-            self.offerLineSeries.append(time, prev.y())
-            self.offerLineSeries.append(time, record.value('Offers'))
-
-        self.offerLineSeries.remove(0)
-
+        self.loadHistoryUntil(time)
         self.resetAxes()
 
+    def loadHistoryUntil(self, cutoff=QDateTime.fromTime_t(0, Qt.UTC)):
+
+        if self.rankPoints.count():
+            last = self.rankPoints.at(self.rankPoints.count() - 1).x()
+        else:
+            last = QDateTime.currentDateTime().toMSecsSinceEpoch()
+
+        earliest = QDateTime.fromMSecsSinceEpoch(last, Qt.LocalTime)
+        earliest = earliest.toTimeSpec(Qt.UTC)
+        earliest = earliest.toTime_t()
+
+        cutoff = cutoff.toTimeSpec(Qt.UTC)
+        cutoff = cutoff.toTime_t()
+
+        for row in range(self.rankPoints.count(), self.model.rowCount()):
+            record = self.model.record(row)
+
+            time = record.value('Timestamp')
+            if time >= earliest:
+                continue
+            elif time < cutoff:
+                return
+
+            time = QDateTime.fromTime_t(time, Qt.UTC)
+            time.toTimeSpec(Qt.LocalTime)
+            time = time.toMSecsSinceEpoch()
+
+            self.rankPoints.append(time, record.value('SalesRank'))
+            self.rankLine.append(time, record.value('SalesRank'))
+            self.pricePoints.append(time, record.value('Price'))
+            self.priceLine.append(time, record.value('Price'))
+            self.offerPoints.append(time, record.value('Offers'))
+
+            if self.offerLine.count() > 1:
+                prev = self.offerLine.at(self.offerLine.count() - 1)
+                self.offerLine.append(prev.x(), record.value('Offers'))
+
+            self.offerLine.append(time, record.value('Offers'))
+
     def resetAxes(self):
-        timemin = QDateTime.fromTime_t(self.model.min('Timestamp')).addDays(-1)
-        timemax = QDateTime.fromTime_t(self.model.max('Timestamp')).addDays(1)
+        r = self.rankPoints.pointsVector()
+        p = self.pricePoints.pointsVector()
+        o = self.offerPoints.pointsVector()
 
-        self.timeAxis.setMin(timemin.toLocalTime())
-        self.timeAxis.setMax(timemax.toLocalTime())
+        # If there is only one data point, set the min and max to the day before and the day after.
+        if len(r) == 1:
+            tmin = QDateTime.fromMSecsSinceEpoch(r[0].x(), Qt.LocalTime).addDays(-1)
+            tmax = QDateTime.fromMSecsSinceEpoch(r[0].x(), Qt.LocalTime).addDays(1)
+        else:
+            tmin = min(r, key=lambda pt: pt.x(), default=QPointF(QDateTime.currentDateTime().addDays(-1).toMSecsSinceEpoch(), 0)).x()
+            tmax = max(r, key=lambda pt: pt.x(), default=QPointF(QDateTime.currentDateTime().addDays(1).toMSecsSinceEpoch(), 0)).x()
+            tmin = QDateTime.fromMSecsSinceEpoch(tmin, Qt.LocalTime)
+            tmax = QDateTime.fromMSecsSinceEpoch(tmax, Qt.LocalTime)
 
-        self.rankAxis.setMin(0)
-        self.rankAxis.setMax((self.model.max('SalesRank') * 1.1 // 1000 + 1) * 1000)
+        self.timeAxis.setMin(tmin)
+        self.timeAxis.setMax(tmax)
 
-        self.priceAxis.setMin((self.model.min('Price') // 5 - 1) * 5)
-        self.priceAxis.setMax((self.model.max('Price') // 5 + 1) * 5)
+        # Find the minimum and maximum values in each series
+        srmin = min(r, key=lambda pt: pt.y(), default=QPointF(0, 0))
+        srmax = max(r, key=lambda pt: pt.y(), default=QPointF(0, 0))
 
-        self.offersAxis.setMin(0)
-        self.offersAxis.setMax((self.model.max('Offers') * 1.1 // 5 + 1) * 5)
+        pmin = min(p, key=lambda pt: pt.y(), default=QPointF(0, 0))
+        pmax = max(p, key=lambda pt: pt.y(), default=QPointF(0, 0))
+
+        omin = min(o, key=lambda pt: pt.y(), default=QPointF(0, 0))
+        omax = max(o, key=lambda pt: pt.y(), default=QPointF(0, 0))
+
+        # Scale the mins and maxes to 'friendly' values for the axes
+        scalemin = lambda v, step: ((v - step / 2) // step) * step
+        scalemax = lambda v, step: ((v + step / 2) // step + 1) * step
+        onlypositive = lambda v: v if v >= 0 else 0
+
+        srmin = onlypositive(scalemin(srmin.y(), 1000))
+        srmax = scalemax(srmax.y(), 1000)
+
+        pmin = onlypositive(scalemin(pmin.y(), 5))
+        pmax = scalemax(pmax.y(), 5)
+
+        omin = onlypositive(scalemin(omin.y(), 2))
+        omax = scalemax(omax.y(), 5)
+
+        # Set the axes' mins and maxes
+        self.rankAxis.setMin(srmin)
+        self.rankAxis.setMax(srmax)
+
+        self.priceAxis.setMin(pmin)
+        self.priceAxis.setMax(pmax)
+
+        self.offersAxis.setMin(omin)
+        self.offersAxis.setMax(omax)
 
     def sceneEvent(self, event):
         if event.type() == QEvent.GraphicsSceneWheel and event.orientation() == Qt.Vertical:
